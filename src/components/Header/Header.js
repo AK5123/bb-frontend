8c060190e06216a8a7fb08ac216d96b2c6d4a9f3
Mohan pierce <paying.rich6990@gmail.com>
2022-02-09 12:18:06 +0530

    push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,19 +10,19 @@ const Header = ({ onSideNavClick }) => {
       <Box
         className="nav-tabs"
         sx={{
-          justifyContent: "space-between",
+          justifyContent: "flex-end",
           alignItems: "center",
           width: "20%",
         }}
       >
-        <a href="https://discord.gg/cbzSvhz3">
+        <a href="https://discord.gg/cbzSvhz3" className="three">
           <p className="caption-bb head">Discord</p>
         </a>
-        <a href="https://t.me/battlebrains">
+        <a href="https://t.me/battlebrains" className="three">
           <p className="caption-bb head">Telegram</p>
         </a>
         <Box className="button-container">
-          <a href="https://tally.so/r/wkxPjn">
+          <a href="https://tally.so/r/wkxPjn" className="eight">
             <p className="button-text-bb disabled">Launch App</p>
           </a>
         </Box>
